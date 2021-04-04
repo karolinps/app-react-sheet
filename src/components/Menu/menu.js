2a7e0a432fa1389fbf6c3d0d94b778c157811934
Karolin APS <porrask92@gmail.com>
2021-04-03 22:10:46 -0500

    menu fixed

---
+++ src/components/Menu/menu.js
@@ -0,0 +1,213 @@
+import React from "react";
+export const data = [
+  {
+    title: "Portafolio",
+    icon: (
+      <svg
+        width="140"
+        height="119"
+        viewBox="0 0 140 119"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="66.9145"
+          cy="88.9145"
+          r="29.9145"
+          fill="#0060D9"
+          fillOpacity="0.8"
+        />
+        <circle
+          cx="110.085"
+          cy="31.1111"
+          r="29.9145"
+          fill="#0060D9"
+          fill0pacity="0.8"
+        />
+        <circle
+          cx="11.9658"
+          cy="92.1368"
+          r="11.9658"
+          fill="#0060D9"
+          fill0pacity="0.8"
+        />
+        <circle
+          cx="49.06"
+          cy="17.9487"
+          r="17.9487"
+          fill="#0060D9"
+          fill0pacity="0.8"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Embudo",
+    icon: (
+      <svg
+        width="160"
+        height="118"
+        viewBox="0 0 160 118"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M80 117.264L56.8079 117.264V83.269L0 0H160L103.713 82.5047V117.264L80 117.264Z"
+          fill="#317DDE"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Estaciones",
+    icon: (
+      <svg
+        width="120"
+        height="118"
+        viewBox="0 0 120 118"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect y="82.1476" width="35.4362" height="35.4362" fill="#317DDE" />
+        <rect
+          x="41.8792"
+          y="82.1476"
+          width="36.2416"
+          height="35.4362"
+          fill="#317DDE"
+        />
+        <rect
+          x="84.5637"
+          y="82.1476"
+          width="35.4362"
+          height="35.4362"
+          fill="#317DDE"
+        />
+        <rect y="41.0738" width="35.4362" height="35.4362" fill="#317DDE" />
+        <rect
+          x="41.8792"
+          y="41.0738"
+          width="36.2416"
+          height="35.4362"
+          fill="#317DDE"
+        />
+        <rect
+          x="84.5637"
+          y="41.0738"
+          width="35.4362"
+          height="35.4362"
+          fill="#317DDE"
+        />
+        <rect width="35.4362" height="35.4362" fill="#317DDE" />
+        <rect x="41.8792" width="36.2416" height="35.4362" fill="#317DDE" />
+        <rect x="84.5637" width="35.4362" height="35.4362" fill="#317DDE" />
+      </svg>
+    ),
+  },
+
+  {
+    title: "Proyectos",
+    icon: (
+      <svg
+        width="130"
+        height="116"
+        viewBox="0 0 130 116"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0 0H37.7273L48.1818 15.9091H130V115.455H0V0Z"
+          fill="#317DDE"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Clientes",
+    icon: (
+      <svg
+        width="145"
+        height="120"
+        viewBox="0 0 145 120"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M121.778 0H24V102.222H0V120H144.889V102.222H121.778V0Z"
+          fill="#317DDE"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Indicadores",
+    icon: (
+      <svg
+        width="120"
+        height="120"
+        viewBox="0 0 120 120"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M89.5904 44.7952C89.5904 53.7203 86.9803 62.0357 82.4817 69.0198L120 106.538L106.538 120L69.0198 82.4817C62.0357 86.9803 53.7203 89.5904 44.7952 89.5904C20.0555 89.5904 0 69.5349 0 44.7952C0 20.0555 20.0555 0 44.7952 0C69.5349 0 89.5904 20.0555 89.5904 44.7952ZM44.7952 73.9121C60.2575 73.9121 72.7922 61.3774 72.7922 45.9151C72.7922 30.4528 60.2575 17.9181 44.7952 17.9181C29.3328 17.9181 16.7982 30.4528 16.7982 45.9151C16.7982 61.3774 29.3328 73.9121 44.7952 73.9121Z"
+          fill="#0060D9"
+          fillOpacity="0.8"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Problemas",
+    icon: (
+      <svg
+        width="156"
+        height="120"
+        viewBox="0 0 156 120"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect y="39.0698" width="42.9268" height="80.9302" fill="#317DDE" />
+        <rect x="55.6099" width="43.9024" height="120" fill="#317DDE" />
+        <rect
+          x="112.195"
+          y="31.6279"
+          width="42.9268"
+          height="88.3721"
+          fill="#317DDE"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Equipos",
+    icon: (
+      <svg
+        width="87"
+        height="120"
+        viewBox="0 0 87 120"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="43.0579"
+          cy="33.0579"
+          r="33.0579"
+          fill="#0060D9"
+          fillOpacity="0.8"
+        />
+        <path
+          d="M87 112C76.5 118 60 119.835 43.1736 119.835C24.9162 119.835 12 119 0.5 112C0.5 93.7427 24.9162 66.1157 43.1736 66.1157C61.4309 66.1157 87 93.7427 87 112Z"
+          fill="#0060D9"
+          fillOpacity="0.8"
+        />
+      </svg>
+    ),
+  },
+];
